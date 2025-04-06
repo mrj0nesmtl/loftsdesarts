d5ec87ec4a037bad3b84155de611f45b48739b8c
Joel <joel.yaffe@gmail.com>
2025-04-06 03:43:55 -0400

    Update Google Maps to 3D view on homepage and add matching map to contact page

--- src/app/(pages)/contact/page.tsx
+++ src/app/(pages)/contact/page.tsx
@@ -69,6 +69,20 @@ export default function ContactPage() {
                 Montréal, Québec<br />
                 Canada H2X 2R5
               </p>
+              
+              {/* Google Maps iframe */}
+              <div className="aspect-video w-full max-w-md mb-6 overflow-hidden rounded-lg">
+                <iframe 
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d356.6934507153928!2d-73.56677393564334!3d45.508200446077666!2m3!1f0!2f39.394737510756755!3f0!3m2!1i1024!2i768!4f35!3m3!1m2!1s0x4cc91a4c1b389f6d%3A0x1d0b7483b40ddc56!2sLofts%20des%20Arts!5e1!3m2!1sen!2sca!4v1743925245718!5m2!1sen!2sca" 
+                  width="100%" 
+                  height="100%" 
+                  style={{ border: 0 }} 
+                  allowFullScreen 
+                  loading="lazy" 
+                  referrerPolicy="no-referrer-when-downgrade"
+                ></iframe>
+              </div>
+              
               <div className="flex space-x-3">
                 <a 
                   href="https://www.facebook.com/LoftDesArts" 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -144,7 +144,7 @@ export default function Home() {
           
           <div className="aspect-video w-full max-w-4xl mx-auto overflow-hidden rounded-lg">
             <iframe 
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2795.9652738541904!2d-73.56930872372341!3d45.51077767107482!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cc91a4c1b389f6d%3A0x1d0b7483b40ddc56!2sLofts%20des%20Arts!5e0!3m2!1sen!2sca!4v1743707624170!5m2!1sen!2sca" 
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d356.6934507153928!2d-73.56677393564334!3d45.508200446077666!2m3!1f0!2f39.394737510756755!3f0!3m2!1i1024!2i768!4f35!3m3!1m2!1s0x4cc91a4c1b389f6d%3A0x1d0b7483b40ddc56!2sLofts%20des%20Arts!5e1!3m2!1sen!2sca!4v1743925245718!5m2!1sen!2sca" 
               width="100%" 
               height="100%" 
               style={{ border: 0 }} 
