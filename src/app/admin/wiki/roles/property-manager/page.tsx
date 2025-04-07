81f3706f964cd39be66a304ba23149a45a81b664
Joel <joel.yaffe@gmail.com>
2025-04-07 15:06:48 -0400

    Fix card import casing in wiki pages

--- src/app/admin/wiki/roles/property-manager/page.tsx
+++ src/app/admin/wiki/roles/property-manager/page.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from 'react';
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/Card";
 import { Badge } from "@/components/ui/badge";
 import { Separator } from "@/components/ui/separator";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
